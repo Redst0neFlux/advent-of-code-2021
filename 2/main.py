2f8e93e56c1e50b17a62022d91208143fbded04b
Redstone Flux <69603438+Redst0neFlux@users.noreply.github.com>
2021-12-03 11:10:34 +0000

    Day 2 complete + created gitignore

--- 2/main.py
+++ 2/main.py
@@ -1,5 +1,7 @@
 with open("input.txt") as f:
     file = f.read().splitlines()
+
+#part one
 depth = 0
 horizontal = 0
 for command in file:
@@ -10,3 +12,17 @@ for command in file:
     elif command.startswith("down"):
         depth += int(command[-1])
 print(depth * horizontal)
+
+#part two
+horizontal_2 = 0
+aim = 0
+depth_2 = 0
+for command in file:
+    if command.startswith("forward"):
+        horizontal_2 += int(command[-1])
+        depth_2 += aim * int(command[-1])
+    elif command.startswith("up"):
+        aim -= int(command[-1])
+    elif command.startswith("down"):
+        aim += int(command[-1])
+print(depth_2 * horizontal_2)
